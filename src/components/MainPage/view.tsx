c636eb66bcb1cd7b35771cc863371541cf725969
nataliaev <nataliiaevchenko@gmail.com>
2019-10-10 15:05:42 +0200

    Add checking repititions algorithm

--- src/components/MainPage/view.tsx
+++ src/components/MainPage/view.tsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { Country } from '../../entities/Country'
 import './styles.css'
 
-export default function MainPage({ countriesInGame, task, answer, handleSubmit, handleChange }: any) {
+export default function MainPage({ countriesInGame, task, answer, error, lastLetter, handleSubmit, handleChange }: any) {
   if (task) {
     return (
       <div className="flexbox">
         <div className="form">
           <h2 className="form-title">Countries already used:</h2>
-          {countriesInGame.map((country: Country) => <div className="country-list" key={country.name}>
-            <img className="small-flag" src={country.flag} alt={country.name} />
-            <span className="list-country">{country.name}</span>
-          </div>)}
+          {countriesInGame.map((country: Country) => {
+            return (
+              <div className="country-list" key={country.name}>
+                <img className="small-flag" src={country.flag} alt={country.name} />
+                <span className="list-country">{country.name}</span>
+              </div>
+            )
+          }
+          )}
         </div>
         <div className="country" >
           <div className="form-title">Computer answers</div>
           <div className="task">
             <img src={task.flag} alt={task.name} />
-            <p>{task.name}</p>
+            <p className="task-name">{task.name}</p>
           </div>
           <div className="form-title">Your answer</div>
           <form className="form" onSubmit={handleSubmit}>
@@ -25,14 +30,14 @@ export default function MainPage({ countriesInGame, task, answer, handleSubmit, 
               className="input-form"
               type="text"
               name="answer"
-              placeholder="Country"
+              placeholder={`Country starting from ${lastLetter}`}
               value={answer}
               onChange={handleChange}
             />
+            <p className="error">{error}</p>
             <input className="input-button" type="submit" value="Submit" />
           </form>
         </div>
-
       </div>
     )
   } else {
